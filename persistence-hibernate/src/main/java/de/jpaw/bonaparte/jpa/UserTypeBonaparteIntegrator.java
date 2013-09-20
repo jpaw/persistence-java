ea552f07fe5d281b1a46489a880fa461fa2dcf97
Michael Bischoff <jpaw@online.de>
2013-09-20 20:22:14 +0200

    support of UUID on postgres with Hibernate

--- persistence-hibernate/src/main/java/de/jpaw/bonaparte/jpa/UserTypeBonaparteIntegrator.java
+++ persistence-hibernate/src/main/java/de/jpaw/bonaparte/jpa/UserTypeBonaparteIntegrator.java
@@ -5,10 +5,15 @@ import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.integrator.spi.Integrator;
 import org.hibernate.metamodel.source.MetadataImplementor;
 import org.hibernate.service.spi.SessionFactoryServiceRegistry;
+import org.hibernate.type.PostgresUUIDType;
+import org.hibernate.type.UUIDBinaryType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.jpaw.util.ByteArray;
 
 public class UserTypeBonaparteIntegrator implements Integrator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserTypeBonaparteIntegrator.class);
 
     @Override
     public void disintegrate(SessionFactoryImplementor sessionFactory, SessionFactoryServiceRegistry serviceRegistry) {
@@ -18,6 +23,15 @@ public class UserTypeBonaparteIntegrator implements Integrator {
     @Override
     public void integrate(Configuration configuration, SessionFactoryImplementor sessionFactory, SessionFactoryServiceRegistry serviceRegistry) {
         configuration.registerTypeOverride(new ByteArrayUserType(), new String[] { ByteArray.class.getName() });
+        String dialect = configuration.getProperty("hibernate.dialect");
+        if ("org.hibernate.dialect.PostgreSQLDialect".equals(dialect)) {
+            LOGGER.info("Postgres DB configured - using special UUID DB type");
+            // configuration.registerTypeOverride(new PostgresUUIDType());
+            configuration.registerTypeOverride(new UuidType());  // must have "java.util.UUID" as getName()! 
+        } else {
+            LOGGER.info("DB type is " + ( dialect == null ? "null" : dialect) + " - using binary UUID DB type");
+            configuration.registerTypeOverride(new UUIDBinaryType());
+        }
     }
 
     @Override
---
+++ persistence-hibernate/src/main/java/de/jpaw/bonaparte/jpa/UuidType.java
@@ -0,0 +1,20 @@
+package de.jpaw.bonaparte.jpa;
+
+import java.util.UUID;
+
+import org.hibernate.type.AbstractSingleColumnStandardBasicType;
+import org.hibernate.type.PostgresUUIDType;
+import org.hibernate.type.descriptor.java.UUIDTypeDescriptor;
+
+/** Special UUID type for Postgres - using the name java.util.UUID */
+public class UuidType extends AbstractSingleColumnStandardBasicType<UUID> {
+ 
+    public UuidType() {
+        super(PostgresUUIDType.PostgresUUIDSqlTypeDescriptor.INSTANCE, UUIDTypeDescriptor.INSTANCE);
+    }
+ 
+    @Override
+    public String getName() {
+        return "java.util.UUID";
+    }
+}
