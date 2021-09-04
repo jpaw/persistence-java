f3561b6e21dc0ece5b97b5b7db4246218179c428
Michael Bischoff <jpaw@online.de>
2021-09-04 23:54:38 +0200

    add JPA converters for FP

---
+++ persistence-core/src/main/java/de/jpaw/bonaparte/jpa/converters/ConverterFpMicroUnits.java
@@ -0,0 +1,30 @@
+package de.jpaw.bonaparte.jpa.converters;
+
+import java.math.BigDecimal;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+import de.jpaw.fixedpoint.types.MicroUnits;
+
+@Converter(autoApply = true)
+public class ConverterFpMicroUnits implements AttributeConverter<MicroUnits, BigDecimal> {
+
+    @Override
+    public BigDecimal convertToDatabaseColumn(MicroUnits data) {
+        if (data == null)
+            return null;
+        return BigDecimal.valueOf(data.getMantissa(), data.getScale());
+    }
+
+    @Override
+    public MicroUnits convertToEntityAttribute(BigDecimal rawData) {
+        if (rawData == null)
+            return null;
+        if (rawData.signum() == 0)
+            return MicroUnits.ZERO;
+        if (rawData.compareTo(BigDecimal.ONE) == 0)
+            return MicroUnits.ONE;
+        return MicroUnits.of(rawData);
+    }
+}
